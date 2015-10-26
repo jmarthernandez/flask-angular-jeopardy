cc0bdec67b84bb95010086c7208efb7e34b83141
Justin Hernandez <jmarthernandez@gmail.com>
2015-10-25 23:10:42 -0500

    Add factory methods to question ctrl

--- static/js/questionCtrl.js
+++ static/js/questionCtrl.js
@@ -1,4 +1,15 @@
 var app = angular.module('myApp');
 
 app.controller('QuestionController', ['$scope', 'Question', function($scope, Question) {
+  $scope.getQuestion = function () {
+    Question.retrieve()
+      .success(function (question) {
+        $scope.question = question[0]
+      })
+      .error(function (err) {
+        console.error(err)
+      });
+  }
+
+  $scope.getQuestion()
 }]);
